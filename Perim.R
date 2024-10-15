b92c25d48e6cc3ed4ec1f0f73aa665613c83e963
rojuabu <rojuabu@alumni.uv.es>
2024-10-15 12:48:29 +0200

    Perímetro rectángulo y cambio PI

--- Perim.R
+++ Perim.R
@@ -5,7 +5,12 @@ PerimetroCuadrado <- function(L){
 
 LongitudCircunferencia <- function(r){
   
-  return(2*pi*r)
+  return(2*3.14159*r)
   
   
+}
+
+PerimetroRectangulo <- function(b,h){
+  
+  return(2*b + 2*h)
 }
